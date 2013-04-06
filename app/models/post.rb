0b9ad779ced3084fa332e226e7a33493394c1007
yeraypv <yerapv@gmail.com>
2013-04-06 11:24:34 +0100

    Autentificacion

---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ActiveRecord::Base
+  attr_accessible :body, :title, :category
+
+  validates :title, :category, :presence => true
+  validates :title, :uniqueness => true
+
+  before_validation :apply_title_format
+
+  has_many :comments
+
+private
+
+  def apply_title_format
+    self[:title] = self[:title].strip.capitalize
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniBlog::Application.config.secret_token = 'a07af677b6113ba36f0f692269757c2867d13ae6167ad29b480f50e32151a82ac40b7cf6f60deb8d4967d0a768f2463dc3d1d16e6092aae8f53d30dbdbc5691c'
